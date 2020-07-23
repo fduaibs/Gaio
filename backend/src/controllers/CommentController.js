c4142ef0e904b864085ac2f8fdbf1563d859f05d
Felipe Duaibs <felipe.duaibs@hotmail.com>
2020-07-23 17:52:39 -0300

    Rota para exclusão de comentários e método destroy

--- backend/src/controllers/CommentController.js
+++ backend/src/controllers/CommentController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const Comment = require('../models/Comment');
+const { destroy } = require('../models/User');
 
 module.exports = {
   async index(req, res) {
@@ -31,4 +32,27 @@ module.exports = {
 
     return res.json(comment); 
   },
+  async destroy(req, res) {
+    const { user_id, comment_id } = req.params;
+
+    const user = await User.findByPk(user_id);
+    if(!user) {
+      return res.status(400).json({ error: 'user not found' });
+    }
+
+    const comment = await Comment.findByPk(comment_id);
+    if(!comment) {
+      return res.status(400).json({ error: 'comment not found' });
+    }
+
+    const deletedCount = await Comment.destroy({ 
+      where: {
+        id: comment_id,
+      }
+    });
+    if(!deletedCount) {
+      return res.status(400).json({ error: 'could not delete comment' });
+    }
+    return res.json(deletedComment);
+  }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -14,6 +14,7 @@ routes.post('/users', UserController.store);
 
 routes.get('/users/:user_id/comments', CommentController.index);
 routes.post('/users/:user_id/comments', CommentController.store);
+routes.delete('/users/:user_id/comments/:comment_id', CommentController.destroy);
 
 
 module.exports = routes;
