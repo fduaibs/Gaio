a43e6ef531a40b38b9da0e63a4abdc8751d8e3b5
Felipe Duaibs <felipe.duaibs@hotmail.com>
2020-07-24 19:16:04 -0300

    Adicionando o tema do MUI, rotas e config do axios

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#98bc6f",
+    },
+    secondary: {
+      main: "#444521",
+    },
+  },
+});
+
+export default theme;
